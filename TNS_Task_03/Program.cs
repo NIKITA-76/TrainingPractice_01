8f8e2f17fb2eea9ba58d5f72435ef4853dfc1bb9
NIKITA-76 <73834676+NIKITA-76@users.noreply.github.com>
2020-11-05 17:01:17 +0300

    Update Program.cs

--- TNS_Task_03/Program.cs
+++ TNS_Task_03/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -10,11 +10,11 @@ namespace TNS_Task_03
     class Program
     {
         
-public static int t = 2;
+public static int t = 2; // Кол-во попыток (Взято 2, так как Первая попытка была использована)
         static void Main(string[] args)
         { 
           
-           string N76 = "N76";
+           string N76 = "N76"; // Пароль
            
             
             
@@ -43,14 +43,18 @@ public static int t = 2;
 
 
             }
-        Finish_bad:
-            Console.WriteLine();
-            Console.WriteLine("Вы привысили количесвто попыток  \n");
-            Console.ReadKey();
+        
         Finish_good:
             Console.WriteLine();
             Console.WriteLine("Пароль верен, Добро пожаловать  \n");
             Console.ReadKey();
+            Environment.Exit(0);
+
+        Finish_bad:
+            Console.WriteLine();
+            Console.WriteLine("Вы привысили количесвто попыток  \n");
+            Console.ReadKey();
+            Environment.Exit(0);
 
 
 
